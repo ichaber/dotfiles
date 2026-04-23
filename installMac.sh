3ebb502aa21ef5de587b1cb3deff84567bee034b
Karl Bergthaler <kbergthaler@intelliad.de>
2026-04-24 00:42:07 +1200

    Add ncurses to Mac install script

--- installMac.sh
+++ installMac.sh
@@ -4,7 +4,7 @@ $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 
 brew update
 brew upgrade
-brew install tmux neovim eza fzf bat lua jq z docker
+brew install tmux neovim eza fzf bat lua jq z ncurses docker
 brew cleanup
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
