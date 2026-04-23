8cbccd29819e26176853c36479d65b252c8dd764
Karl Bergthaler <kbergthaler@intelliad.de>
2026-04-24 11:03:29 +1200

    Tmux: Use TPM from .config location

--- installMac.sh
+++ installMac.sh
@@ -11,7 +11,8 @@ brew upgrade
 brew install tmux neovim mise eza fzf bat lua jq z ncurses docker
 brew cleanup
 
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+mkdir -p ~/.config/tmux/plugins/tpm
 mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 git clone -b v2.3.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
 
